d393982f8100dd79d51326e1637a8723a47655f4
Rohan Marde <rohanmarde@campus-028-013.ucdavis.edu>
2024-11-14 12:22:32 -0800

    nothing new really

--- wordCount.py
+++ wordCount.py
@@ -1,20 +1,21 @@
 import argparse
 import sys
 
-parser = argparse.ArgumentParser(
-    description = "count the word 'the' in a text file"
-)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description = "count the word 'the' in a text file"
+    )
 
-parser.add_argument('--input', default=None, metavar='<str>', help="the input text")
+    parser.add_argument('--input', default=None, metavar='<str>', help="the input text")
 
-arg = parser.parse_args()
+    arg = parser.parse_args()
 
-inp = arg.input if arg.input else sys.stdin.read()
+    inp = arg.input if arg.input else sys.stdin.read()
 
-count = 0
+    count = 0
 
-for i in inp.split():
-    if i == "the":
-        count += 1
+    for i in inp.split():
+        if i == "the":
+            count += 1
 
-print(count)
+    print(count)
